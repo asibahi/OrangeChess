595a42ee61fa8cd153d3f5321a82726d6ff1712e
asibahi <a.sibahi@gmail.com>
2016-06-13 14:37:11 -0500

    First Commit

---
+++ OrangeChess/Square.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrangeChess
+{
+    /// <summary>
+    /// Indicates a position on the board. However, it is used to define Moves, valid
+    /// or invalid, not to actually indicate Squares on the Board, which uses a [,].
+    /// Ideally in future versions of C# this would be a record type, or a data class.
+    /// </summary>
+    public struct Square : IEquatable<Square>
+    {
+        const string _fileValues = "abcdefghjklmnopqrstuvwxyz"; // no i
+        byte _file; // starts from 0 at the Queenside to whatever on the Kingside
+        byte _rank; // starts from 0 at White's side  to whatever on Black's side
+
+        /// <summary>
+        /// Square constructor based on file and rank, in algebraic notation.
+        /// </summary>
+        /// <param name="file">File, or Column on the board.</param>
+        /// <param name="rank">Rank, or Row on the board.</param>
+        public Square(char file, byte rank) : this()
+        {
+            File = file;
+            Rank = rank;
+        }
+
+        /// <summary>
+        /// Square constructor based on file and rank in string format, in algebraic notation.
+        /// </summary>
+        /// <param name="position">Algebraic location of the square.</param>
+        public Square(string position) : this()
+        {
+            byte tmp;
+            if(string.IsNullOrWhiteSpace(position) || position.Length < 2 || !byte.TryParse(position.Substring(1), out tmp))
+                throw new ArgumentException("Algebraic notation format not recognized.");
+
+            File = position[0];
+            Rank = tmp;
+        }
+
+        /// <summary>
+        /// File, or Column. For example in Chess the King starts on the e file.
+        /// </summary>
+        public char File
+        {
+            get
+            {
+                return _fileValues[_file];
+            }
+
+            private set
+            {
+                if(char.IsLetter(value) && _fileValues.ToCharArray().Contains(char.ToLower(value)))
+                    _file = (byte)_fileValues.IndexOf(char.ToLower(value));
+                else
+                    throw new ArgumentException("File must be an English letter other than 'i'");
+            }
+        }
+
+        /// <summary>
+        /// Rank, or Row. For example in Chess the pawns start on the 2nd rank.
+        /// </summary>
+        public byte Rank
+        {
+            get
+            {
+                // to correct for 0 being the default value
+                return (byte)(_rank + 1);
+            }
+
+            private set
+            {
+                if(value <= 0 || value > 26)
+                    throw new ArgumentException("Rank must be between 1 and 26 inclusive");
+
+                _rank = (byte)(value - 1);
+            }
+        }
+
+        /// <summary>
+        /// Overrides Object.ToString()
+        /// </summary>
+        /// <returns>The File and Rank in Algebraic notation.</returns>
+        public override string ToString() => (File + Rank.ToString());
+
+        /// <summary> Value Equality based on Rank and File. </summary>
+        /// <param name="other">The other Square</param>
+        /// <returns>True if both Squares share the rank and file. (Wouldn't they be technically the same square?)</returns>
+        public bool Equals(Square other) => _rank == other._rank && _file == other._file;
+
+        /// <summary>Indicates whether this instance and a specified object are equal.</summary>
+        /// <returns>true if <paramref name="obj" /> and this instance are the same type and represent the same value; otherwise, false. </returns>
+        /// <param name="obj">The object to compare with the current instance. </param>
+        /// <filterpriority>2</filterpriority>
+        public override bool Equals(object obj) => (obj is Square) && Equals((Square)obj);
+
+        /// <summary>Returns the hash code for this instance.</summary>
+        /// <returns>A 32-bit signed integer that is the hash code for this instance.</returns>
+        /// <filterpriority>2</filterpriority>
+        public override int GetHashCode() => (Rank.GetHashCode() * 31) ^ File.GetHashCode();
+
+        public static bool operator ==(Square sq1, Square sq2) => sq1.Equals(sq2);
+        public static bool operator !=(Square sq1, Square sq2) => !sq1.Equals(sq2);
+    }
+}
---
+++ OrangeChessTests/SquareTests.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using NUnit.Framework;
+using OrangeChess;
+
+namespace OrangeChessTests
+{
+    [TestFixture]
+    public class SquareTests
+    {
+        [Test]
+        public void DefaultSquareCotr_EvaluatesToA1()
+        {
+            var sq = new Square();
+
+            Assert.AreEqual("a1", sq.ToString());
+        }
+
+        [TestCase("a2")]
+        [TestCase("h2")]
+        [TestCase("r5")]
+        [TestCase("s26")]
+        [TestCase("l25")]
+        [TestCase("j1")]
+        public void SquareStringCtor_ValidArguments_Works(string str)
+        {
+            var sq = new Square(str);
+
+            Assert.AreEqual(str.ToLower(), sq.ToString());
+        }
+
+        [TestCase("a")]
+        [TestCase("")]
+        [TestCase("5r")]
+        [TestCase("s100")]
+        [TestCase("435")]
+        [TestCase("fd")]
+        [TestCase(null)]
+        [TestCase("$#5")]
+        [TestCase("ب4")]
+        public void SquareStringCtor_InvalidArguments_Throws(string str)
+        {
+            Assert.Throws<ArgumentException>(() => new Square(str));
+        }
+
+        [TestCase('a', 3, "a3")]
+        [TestCase('h', 25, "h25")]
+        [TestCase('D', 20, "d20")]
+        [TestCase('z', 3, "z3")]
+        [TestCase('j', 13, "j13")]
+        [TestCase('a', 1, "a1")]
+        public void SquareFileRankCtor_ValidArguments_Works(char f, byte r, string expected)
+        {
+            var sq = new Square(f, r);
+
+            Assert.AreEqual(expected, sq.ToString());
+        }
+
+        [TestCase('a', 27)]
+        [TestCase('i', 2)]
+        [TestCase('f', 0)]
+        [TestCase('ب', 3)]
+        [TestCase(' ', 1)]
+        public void SquareFileRankCtor_InvalidArguments_Throws(char f, byte r)
+        {
+            Assert.Throws<ArgumentException>(() => new Square(f, r));
+        }
+
+        [TestCase('a', 1, "a1")]
+        [TestCase('b', 2, "b2")]
+        [TestCase('h', 8, "h8")]
+        [TestCase('j', 10, "j10")]
+        public void SquareEquals_EqualArguments_ReturnsTrue(char f, byte r, string str)
+        {
+            var sq1 = new Square(f, r);
+            var sq2 = new Square(str);
+
+            Assert.True(sq1 == sq2);
+            Assert.True(sq1.Equals(sq2));
+            Assert.True(sq1.GetHashCode() == sq2.GetHashCode());
+
+            Assert.False(sq1 != sq2); // I am not sure about multiple Asserts like that
+        }
+    }
+}
